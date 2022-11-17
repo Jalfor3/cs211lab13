912b0baa26b7b3f776173d875ad771fd7415abe0
Jamani Alford <jalfor3@uic.edu>
2022-11-17 16:54:14 +0000

    Lab success

---
+++ .ccls-cache/@home@runner@Lab13/211-f22-collab-github@collab.c
@@ -0,0 +1,31 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void catta2_jalfor3() {
+  printf("Jamani Alford and Ceasar Attar\n");
+  printf("We learned how to commit, pull and push new code through Git. Not only were we able to do that, through replit you're able to do a lot more commands including running terminal and seeing the results of code that you pull through Git/Replit. \n");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    catta2_jalfor3();
+    
+    return 0;
+}
